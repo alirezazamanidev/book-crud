faaae6988fb2d96d6abd1f9b3533a689a6a9beb1
Alireza Zamani <alirezazamanidev80@gmail.com>
2026-02-16 10:09:00 +0330

    feat: implement typeOrm book entitiy

---
+++ src/modules/book/infrastructure/typeorm/book.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('books')
+export class BookEntity {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  title: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ type: 'varchar', length: 13, unique: true })
+  isbn: string;
+
+  @Column({ type: 'varchar', length: 20, default: 'DRAFT' })
+  status: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
